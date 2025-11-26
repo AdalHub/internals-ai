fb1489c55cde8eaacc8cacf0e7587be262b605c4
adalhub <adalteran@gmail.com>
2025-11-26 12:24:15 -0600

    feat: added initial get and post routes for documents

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 my_app = FastAPI()
-
-origins = ["http://localhost:3000"]
+'''
+origins = ["http://localhost:8000"]
 
 my_app.add_middleware(
     CORSMiddleware,
@@ -11,8 +11,26 @@ my_app.add_middleware(
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
-)
+)'''
+#placeholder for database
+documents = []
 
+#test function
 @my_app.get("/")
 def getStuff():
     return {"mgs":"hello world"}
+
+
+#we will post iternal document here
+@my_app.post("/items")
+def createItems(document: str):
+    documents.append(document)
+    return document
+
+#view specific item
+@my_app.get("/items/{itemId}")
+def getItem(itemId: int):
+    if itemId not in range(len(documents)):
+        return "not found"
+    else:
+        return documents[itemId]
