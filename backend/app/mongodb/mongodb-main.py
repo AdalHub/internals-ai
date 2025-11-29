12f3d8e120b1c84e1f6afe0254e1b6139edc8e06
adalhub <adalteran@gmail.com>
2025-11-28 22:19:42 -0600

    test: added some temp test cases

--- backend/app/mongodb/mongodb-main.py
+++ backend/app/mongodb/mongodb-main.py
@@ -1,8 +1,9 @@
 
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
+from pymongo.mongo_client import MongoClient    
+from pymongo.server_api import ServerApi    
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 load_dotenv("../../.env")
 mongodb_username=os.getenv('MONGO_DB_USERNAME')
@@ -20,3 +21,52 @@ try:
     print("Pinged your deployment. You successfully connected to MongoDB!")
 except Exception as e:
     print(e)
+
+
+
+#automatically create a new db
+db =client.internals_ai_db
+
+
+
+#creating users collection
+users_collection= db.users
+
+
+
+
+user_test = {
+    "name": "jose",
+    "age":53,
+    "access_leve": "admin",
+    "created_at": datetime.now()
+}
+result=users_collection.insert_one(user_test)
+print(f"result from test user insert: {result.inserted_id}")
+
+multiple_users = [
+    {
+        "name": "justin",
+        "age":25,
+        "access_leve": "user",
+        "created_at": datetime.now()
+    },
+    {
+        "name": "sam",
+        "age":25,
+        "access_leve": "user",
+        "created_at": datetime.now()
+    },
+    {
+        "name": "alex",
+        "age":24,
+        "access_leve": "user",
+        "created_at": datetime.now()
+    },
+]
+
+result_1= users_collection.insert_many(multiple_users)
+print(f"successfully inserted this many test users: {len(result.inserted_ids)}")
+
+#listing database names
+print(client.list_database_names())
